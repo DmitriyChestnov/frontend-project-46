4eb2b9ecc30b5396236852b3fe90f05a363d80e9
Dmitriy Chestnov <invheor@gmail.com>
2023-05-29 22:53:15 +0300

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,49 +1,43 @@
 import _ from 'lodash';
 
+const getPrefixes = (depth) => {
+  const replacer = ' ';
+  const spacesCount = 4;
+  const prefix = replacer.repeat(spacesCount * (depth + 1) - 2);
+  const bracePrefix = replacer.repeat(spacesCount * depth);
+  return { prefix, bracePrefix };
+};
+
 const stylish = (data, depth = 0) => {
   if (data === undefined || data === null || typeof data !== 'object') {
     return String(data);
   }
-  const replacer = ' ';
-  const spacesCount = 4;
+  let lines;
+  const { prefix, bracePrefix } = getPrefixes(depth);
   const currentDepth = depth + 1;
-  const prefix = replacer.repeat(spacesCount * (depth + 1) - 2);
-  const bracePrefix = replacer.repeat(spacesCount * depth);
-  if (!Array.isArray(data)) {
+  if (Array.isArray(data)) {
+    lines = data.flatMap(({
+      key, val, oldVal, operation,
+    }) => {
+      let result = [];
+      const valStr = stylish(val, currentDepth);
+      if (operation === 'update') {
+        const oldValStr = stylish(oldVal, currentDepth);
+        result = [`${prefix}- ${key}: ${oldValStr}`, `${prefix}+ ${key}: ${valStr}`];
+      } else if (operation === 'add') {
+        result = `${prefix}+ ${key}: ${valStr}`;
+      } else if (operation === 'delete') {
+        result = `${prefix}- ${key}: ${valStr}`;
+      } else if (operation === 'nochange') {
+        result = `${prefix}  ${key}: ${valStr}`;
+      }
+      return result;
+    });
+  } else {
     const keys = _.sortBy(_.keys(data));
-    const lines = keys.map((key) => `${prefix}  ${key}: ${stylish(data[key], currentDepth)}`);
-    return ['{', ...lines, `${bracePrefix}}`].join('\n');
+    lines = keys.map((key) => `${prefix}  ${key}: ${stylish(data[key], currentDepth)}`);
   }
-
-  const lines = data.flatMap(({
-    key, val, oldVal, operation,
-  }) => {
-    let result = [];
-    const valStr = stylish(val, currentDepth);
-    if (operation === 'update') {
-      const oldValStr = stylish(oldVal, currentDepth);
-      result = [
-        `${prefix}- ${key}: ${oldValStr}`,
-        `${prefix}+ ${key}: ${valStr}`,
-      ];
-    } else if (operation === 'add') {
-      result = `${prefix}+ ${key}: ${valStr}`;
-    } else if (operation === 'delete') {
-      result = `${prefix}- ${key}: ${valStr}`;
-    } else if (operation === 'nochange') {
-      result = `${prefix}  ${key}: ${valStr}`;
-    }
-    return result;
-  });
   return ['{', ...lines, `${bracePrefix}}`].join('\n');
 };
 
-const formatDiff = (data, formatter = 'stylish') => {
-  let format;
-  if (formatter === 'stylish') {
-    format = stylish;
-  }
-  return format(data);
-};
-
-export default formatDiff;
+export default stylish;
